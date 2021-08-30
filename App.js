58fe3f92d66409046d87f5193efcea2954461bb6
zakariamosa <zakariamosa1211@gmail.com>
2021-08-31 00:31:11 +0200

    I started to understand a bit more

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+
 
 export default function App() {
+  const [isHungry, setIsHungry] = useState(true);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Meeeeeeeeeeeeeew!</Text>
       <StatusBar style="auto" />
+      <Image
+        source={{uri: "https://reactnative.dev/docs/assets/p_cat1.png"}}
+        style={{width: 200, height: 200}}
+      />
+        <Button
+        onPress={() => {
+          setIsHungry(false);
+        }}
+        disabled={!isHungry}
+        title={isHungry ? "Pour me some milk, please!" : "Thank you!"}
+      />
     </View>
   );
 }
